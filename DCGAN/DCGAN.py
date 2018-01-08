5534529f1dd473b2fd54ed5f6104009b617f5abf
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-01-08 09:44:06 +0900

    Update Generator

--- DCGAN/DCGAN.py
+++ DCGAN/DCGAN.py
@@ -14,4 +14,26 @@ n_noise = 100
 X = tf.placeholder(tf.float32, [None, n_height, n_width, 1])
 Z = tf.placeholder(tf.float32, [None, 1, 1, n_noise])
 
+def Generator(Z, reuse=None, training=True):
+    with tf.variable_scope("Generator", reuse=reuse):
+        G1 = tf.layers.conv2d_transpose(Z, 1024, [4, 4], 1, padding='valid')
+        G1 = tf.layers.batch_normalization(G1, training=training)
+        G1 = tf.nn.leaky_relu(G1)
+
+        G2 = tf.layers.conv2d_transpose(G1, 512, [4, 4], 2, padding='same')
+        G2 = tf.layers.batch_normalization(G2, training=training)
+        G2 = tf.nn.leaky_relu(G2)
+
+        G3 = tf.layers.conv2d_transpose(G2, 256, [4, 4], 2, padding='same')
+        G3 = tf.layers.batch_normalization(G3, training=training)
+        G3 = tf.nn.leaky_relu(G3)
+
+        G4 = tf.layers.conv2d_transpose(G3, 128, [4, 4], 2, padding='same')
+        G4 = tf.layers.batch_normalization(G4, training=training)
+        G4 = tf.nn.leaky_relu(G4)
+
+        G5 = tf.layers.conv2d_transpose(G4, 1, [4, 4], 2, padding='same')
+        output = tf.nn.tanh(G5)
+
+        return output
 
