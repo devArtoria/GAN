369b011ed193a647666e2c69859b671828b99d07
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-01-08 12:15:15 +0900

    Update DCGAN session run

--- DCGAN/DCGAN.py
+++ DCGAN/DCGAN.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./mnist/data", one_hot=True)
+mnist = input_data.read_data_sets("./mnist/data", one_hot=True, reshape=[])
 
 batch_size = 100
 learning_rate = 0.0002
@@ -16,8 +16,8 @@ X = tf.placeholder(tf.float32, [None, n_height, n_width, 1])
 Z = tf.placeholder(tf.float32, [None, 1, 1, n_noise])
 training = tf.placeholder(tf.bool)
 
-def Generator(Z, reuse=None, training=True):
-    with tf.variable_scope("Generator", reuse=reuse):
+def Generator(Z, reuse=False, training=True):
+    with tf.variable_scope("Generator", reuse=tf.AUTO_REUSE):
         G1 = tf.layers.conv2d_transpose(Z, 1024, [4, 4], 1, padding='valid')
         G1 = tf.layers.batch_normalization(G1, training=training)
         G1 = tf.nn.leaky_relu(G1)
@@ -39,8 +39,8 @@ def Generator(Z, reuse=None, training=True):
 
         return output
 
-def Discriminator(X, reuse=None, training=True):
-    with tf.variable_scope("Discriminator", reuse=reuse):
+def Discriminator(X, reuse=False, training=True):
+    with tf.variable_scope("Discriminator", reuse=tf.AUTO_REUSE):
         D1 = tf.layers.conv2d(X, 128, [4, 4], 2, padding='same')
         D1 = tf.nn.leaky_relu(D1)
 
@@ -61,12 +61,10 @@ def Discriminator(X, reuse=None, training=True):
 
         return output, D5
 
-# Z = np.random.normal(0, 1, (25, 1, 1, 100))
+G = Generator(Z, training=training)
 
-G = Generator(Z, training)
-
-D_gene, D_gene_logits = Discriminator(G, training, reuse=True)
-D_real, D_real_logits = Discriminator(X, training)
+D_gene, D_gene_logits = Discriminator(G, True, training)
+D_real, D_real_logits = Discriminator(X, training=training)
 
 loss_D = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, labels=tf.ones([batch_size, 1, 1, 1])) +
                         tf.nn.sigmoid_cross_entropy_with_logits(logits=D_gene_logits, labels=tf.zeros([batch_size, 1, 1, 1])))
@@ -79,13 +77,21 @@ G_var_list = [var for var in train_params if var.name.startswith('G')]
 train_D = tf.train.AdamOptimizer(learning_rate).minimize(loss_D, var_list=D_var_list)
 train_G = tf.train.AdamOptimizer(learning_rate).minimize(loss_G, var_list=G_var_list)
 
-sess = tf.Session()
+sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
 train_set = tf.image.resize_images(mnist.train.images, [64, 64]).eval()
 train_set = (train_set - 0.5) / 0.5
 
+total_batch = int(mnist.train.num_examples / batch_size)
 
+for epoch in range(epoch):
+    for i in range(total_batch):
+        batch_xs = train_set[i*batch_size:(i+1)*batch_size]
+        noise = np.random.normal(0, 1, (batch_size, 1, 1, 100))
 
+        _, loss_val_D = sess.run([train_D, loss_D], feed_dict={X: batch_xs, Z: noise, training: True})
+        _, loss_val_G = sess.run([train_G, loss_G], feed_dict={X: batch_xs, Z: noise, training: True})
 
+        print('Epoch :  %04d' % epoch, 'D loss: {:.4}'.format(loss_val_D), 'G loss: {:.4}'.format(loss_val_G))
 
