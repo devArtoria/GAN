07e2a3f0080dcf4aeb6b798c615436c9b648cc5b
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-01-08 09:43:42 +0900

    Update DCGAN params

--- DCGAN/DCGAN.py
+++ DCGAN/DCGAN.py
@@ -1,97 +1,17 @@
+import tensorflow as tf
 
-#INIT
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./mnist/data", one_hot=True)
 
-'''
-batch size 100
+batch_size = 100
+learning_rate = 0.0002
+epoch = 20
 
-learning rate 0.0002
+n_height = 64
+n_width = 64
+n_noise = 100
 
-epoch 20
-
-leakyReLU 0.2
-
-Adam
-'''
-
-
-'''
-Net config
-
-Discriminator
-
-conv 1
-64*64*1
-4*4 filter 2 stride
-
-conv 2
-32*32*128
-4*4 filter 2 stride
-
-leaky_relu
-
-conv 3
-16*16*256
-4*4 filter 2 stride
-
-bnorm
-
-leaky_relu
-
-conv 4
-8*8*512
-4*4 filter 2 stride
-
-bnorm
-
-leaky_relu
-
-conv 5
-4*4*1024
-4*4 filter 2 stride
-
-bnorm
-
-leaky_relu
-
-sigmoid(1*1 output)
+X = tf.placeholder(tf.float32, [None, n_height, n_width, 1])
+Z = tf.placeholder(tf.float32, [None, 1, 1, n_noise])
 
 
-
-
-Generator
-
-input 100
-
-dconv1
-4*4*1024
-4*4 filter 2 stride
-
-dconv2
-8*8*512
-4*4 filter 2 stride
-
-bnorm
-
-leaky_relu
-
-dconv3
-16*16*256
-4*4 filter 2 stride
-
-bnorm
-
-leaky_relu
-
-dconv4 
-32*32*128
-4*4 filter 2 stride
-
-bnorm
-
-leaky_relu
-
-dconv5
-64*64*1
-
-TanH
-'''
